67c576b0986416033b20c20dfe8eb99199984e0a
kelvin-kimutai <kelvinkpkrr@gmail.com>
2022-04-10 02:09:45 +0300

    Changed redirect url to relative version

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -13,8 +13,6 @@ export async function middleware(req) {
   }
 
   if (!token && pathname !== '/login') {
-    return NextResponse.redirect(
-      'https://spotify-clone-kelvin.vercel.app/login'
-    )
+    return NextResponse.redirect('http://localhost:3000/login')
   }
 }
